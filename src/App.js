470421ca848905dae799abb67f9b2fc59b3c3f08
chaean <jtkim965@naver.com>
2023-01-27 18:20:23 +0900

    useEffect 사용법 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
-  console.log("it's run all the time");
+  const onChage = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("once");
+    console.log("i run only once.");
   }, []);
+  useEffect(() => {
+    console.log("i run when keyword changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when counter changes.");
+  }, [counter]);
   return (
     <div>
+      <input onChange={onChage} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
