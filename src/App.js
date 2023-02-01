b1e6e9ec361c3c9d015dd32960569e2a5860ac00
chaean <jtkim965@naver.com>
2023-02-01 18:12:03 +0900

    코인 api 불러오기 완성

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ function App() {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
+        setCoins(json);
         setLoading(false);
       });
   }, []);
   return (
     <div>
-      <h1>The Coins</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins {loading ? "" : `${coins.length}`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
